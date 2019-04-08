8ebac6f88b5e3ce764c1ec10af8b8d03ebc143fb
qinbinyuan <binyuan_qin@163.com>
2019-04-08 22:39:08 +0800

    修改id的生成逻辑

--- src/main/java/com/didoumi/www/data/utils/NoteUtil.java
+++ src/main/java/com/didoumi/www/data/utils/NoteUtil.java
@@ -9,14 +9,25 @@ import java.util.Random;
 
 public class NoteUtil {
 
-    //随机数发生器Id
+    //随机数发生器Id，生成18位数的id
     public static String getId(){
-        return new Random().nextInt(100000) +""+System.currentTimeMillis();//获得毫秒数加随机数
+        String id = new Random().nextInt(100000) + "" +System.currentTimeMillis();//获得毫秒数加随机数
+        if (id.length() < 18) {
+            int temp = 1;
+            for (int i =0; i < (18 - id.length()); i++) {
+                temp = temp * 10;
+            }
+            id += new Random().nextInt(temp - 1) ;
+        }
+        return id;
     }
 
     //获取session中的值
     public static Object getSessionForKey(HttpServletRequest request, String key) {
         return request.getSession().getAttribute(key);
     }
-
+    public static void main(String[] args) {
+        System.out.println(getId());
+        System.out.println(getId().length());
+    }
 }
